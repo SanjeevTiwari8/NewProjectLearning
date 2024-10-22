6675cab08bb8aa86e102d97392e8a4cac6db8562
Sanjeev <sanjeev.t.4u@gmail.com>
2024-10-23 02:05:38 +0530

    yes

--- src/LearningJava/Lab001.java
+++ src/LearningJava/Lab001.java
@@ -3,5 +3,6 @@ package LearningJava;
 public class Lab001 {
     public static void main(String[] args) {
         System.out.println("Main test");
+        System.out.println("Git test");
     }
 }
